58480c44bb90dedafb59818ccd2c325034183ee9
Yoon <cbcn123@naver.com>
2024-10-22 15:39:00 +0900

    2024-10-22

---
+++ 2024Chap09_NastedClass/src/A.java
@@ -0,0 +1,36 @@
+
+// 중첩 클래스 활용 예시
+
+public class A {
+	A(){
+		System.out.println("A 클래스 생성자 실행");
+	}
+	static class B{
+		B(){
+			System.out.println("B 생성자 실행");
+		}
+		void m1() {
+			System.out.println("B - m1 실행");
+		}
+		static void m2() {
+			System.out.println("B - m2 실행");
+		}
+	}
+	class C{
+		C(){
+			System.out.println("C 생성자 실행");
+		}
+	}
+	
+	public static void main(String[] args) {
+		 // A클래스 안의 B클래스타입으로 b 객체 생성 
+		A.B b = new A.B(); 
+		b.m1();
+		A.B.m2();
+		
+		A a = new A();
+		A.C c = a.new C(); // 밑에거랑 같음 
+		A.C d = new A().new C();
+		
+	}
+}
